8db03f114acaef07b42be1ee61506fcaba98f374
asya hambrick <asya.simone@orange.fr>
2024-11-12 18:34:47 +0100

    fist submition rough draft

---
+++ script.js
@@ -0,0 +1,16 @@
+new Swiper('.card-wrap', {
+  direction: 'horizontal',
+  loop: true,
+
+  // pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
