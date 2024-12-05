29230476cd1881ffccc42f6f99915a05e4f9b842
asya hambrick <asya.simone@orange.fr>
2024-12-05 16:40:35 +0100

    edit of swiper spacing and sizing

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 new Swiper('.card-wrap', {
   direction: 'horizontal',
   loop: true,
-
-
+  slidesPerView: 1,
+  spaceBetween: 10,
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
