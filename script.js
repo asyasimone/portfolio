6974d753c34c91f64d507dd58d993840f21ecf1c
asya hambrick <asya.simone@orange.fr>
2024-12-26 18:42:45 +0100

    tabs coded and slide show content edited

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 new Swiper('.card-wrap', {
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 1,
-  spaceBetween: 10,
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
@@ -10,3 +8,18 @@ new Swiper('.card-wrap', {
   },
 
 });
+
+//about tab
+const tablinks = document.getElementsByClassName('tab-links');
+const tabcontents = document.getElementsByClassName('tab-contents');
+for (let i = 0; i < tablinks.length; i++) {
+  tablinks[i].addEventListener('click', function () {
+      for (let j = 0; j < tablinks.length; j++) {
+          tablinks[j].classList.remove('active-link');
+          tabcontents[j].classList.remove('active-tab');
+      }
+
+      this.classList.add('active-link');
+      tabcontents[i].classList.add('active-tab');
+  });
+}
